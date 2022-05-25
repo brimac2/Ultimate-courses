1b742277944123afb8496b3a4df620e7f7d51b4f
Brigita Macaitė <brigita.macaite@swedbank.lt>
2022-05-25 15:21:52 +0300

    feat: functions, operations added

--- src/app.ts
+++ src/app.ts
@@ -15,6 +15,46 @@ const pizza = {
 };
 console.log(pizza.getName());
 
+
+// Sukurti nauja objekta, panaudojant jau turima objekta
+const drink = {
+    name: 'Fanta',
+    price: 2
+}
+
+const ingredients = ['sugar'];
+
+function createOrder(drink, ingredients) {
+     return {drink, ingredients };
+}
+
+console.log(createOrder(drink, ingredients))
+
+// Kad perduoti kintamajam (variablui) bet kokius skaičius ar ir skačius, message naudojam .reduce
+
+function sumAll(message, ...arr){
+    console.log(message)
+return arr.reduce((prev, next) => prev + next);
+}
+
+const sum = sumAll('Hello', 1,2,3,4,5)
+console.log(sum);
+
+// Spread operator, kai galima isskaidyti objekta i properties (priekyje padedami ...)
+const chocolade = {
+    name: 'milka'
+};
+
+const component = ['sugar'];
+const order = {
+...chocolade, component
+}
+
+const finalOrder = {...order}
+console.log(finalOrder)
+
+
+
 // Number tipas ir panaudijimas funkcijoje
 const pizzaCost: number = 10;
 const pizzaToopings = 6;
